e038b0b26d10ba9662ba7d1b3686fb1d392b0293
himanshu6532 <himanshu.yadav_cs.aiml24@gla.ac.in>
2026-03-20 17:53:32 +0530

    programme commit [Himanshu Yadav]

--- src/modules/fan.h
+++ src/modules/fan.h
@@ -5,4 +5,8 @@
 #ifndef AI_BASED_VOICE_AND_MOTION_CONTROLLED_SMART_LIGHT_SYSTEM_FAN_H
 #define AI_BASED_VOICE_AND_MOTION_CONTROLLED_SMART_LIGHT_SYSTEM_FAN_H
 
+// Add public declarations for fan control
+void initFans();
+void setFan(int id, bool state);
+
 #endif //AI_BASED_VOICE_AND_MOTION_CONTROLLED_SMART_LIGHT_SYSTEM_FAN_H
